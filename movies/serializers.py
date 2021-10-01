a9f778003d73178b06d0559905c4b18f9f996134
Nosspi <nosspireit@gmail.com>
2021-10-01 18:11:23 +0300

    first commit

---
+++ movies/serializers.py
@@ -0,0 +1,86 @@
+from rest_framework import serializers
+from .models import Movie, Review, Rating, Actor
+
+
+class MovieLIstSerializer(serializers.ModelSerializer):
+    """Список фильмов"""
+    rating_user = serializers.BooleanField()
+    middle_star = serializers.IntegerField()
+
+    class Meta:
+        model = Movie
+        fields = ("title", "tagline", 'category',  "rating_user", "middle_star")
+
+
+class FilterReviewLIstSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class ActorListSerializer(serializers.ModelSerializer):
+    """Вывод списка актеров и режиссеров"""
+    class Meta:
+        model = Actor
+        fields = ("id", "name", "image")
+
+
+class ActorDetailSerializer(serializers.ModelSerializer):
+    """Вывод полного описани актера или режиссера"""
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """Добавление рейтинга пользователем"""
+    class Meta:
+        model = Rating
+        fields = ("star", "movie")
+
+    def create(self, validated_data):
+        rating = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get("star")}
+        )
+        return rating
+
+
+class RecursiveSerializer(serializers.ModelSerializer):
+    """Вывод рекурсивно children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """Создание отзывов"""
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Вывод отзыва"""
+    children = RecursiveSerializer(many=True)
+
+    class Meta:
+        list_serializer_class = FilterReviewLIstSerializer
+        model = Review
+        fields = ("name", "text", "children")
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """Детали фильма"""
+
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    directors = ActorListSerializer(read_only=True, many=True)
+    actors = ActorListSerializer(read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        exclude = ('draft',)
---
+++ movies/views.py
@@ -0,0 +1,64 @@
+from django.db import models
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Movie
+from .serializers import *
+from .service import get_client_ip
+
+
+class MovieListView(APIView):
+    """Вывод списка фильмов """
+
+    def get(self, request):
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
+        serializer = MovieLIstSerializer(movies, many=True)
+        return Response(serializer.data)
+
+
+class MovieDetailView(APIView):
+    """Вывод фильма """
+
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk, draft=False)
+        serializer = MovieDetailSerializer(movie)
+        return Response(serializer.data)
+
+
+class ReviewCreateView(APIView):
+    """Добавление отзыва к фильму"""
+
+    def post(self, request):
+        review = ReviewCreateSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+        return Response(status=201)
+
+
+class AddStarRatingView(APIView):
+    """Добавление рейтинга фильму"""
+
+    def post(self, request):
+        serializer = CreateRatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(ip=get_client_ip(request))
+            return Response(status=201)
+        else:
+            return Response(status=400)
+
+
+class ActorsListView(generics.ListAPIView):
+    """Вывод списка актеров"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorListSerializer
+
+
+class ActorsDetailView(generics.RetrieveAPIView):
+    """Вывод актера или режиссера"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializer
